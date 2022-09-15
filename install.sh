2fb6a0940826b6ac2e37b60c8a046b1452f9b9a7
DrgnFireYellow <86269995+DrgnFireYellow@users.noreply.github.com>
2022-09-15 18:00:13 +0000

    fix installer

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "Installing zlugin"
 mkdir ~/.zlugin 2>> $HOME/zlugin.log
-curl https://raw.githubusercontent.com/DrgnFireYellow/zlugin/main/zlugin.zsh > $HOME/.zlugin/zlugin.zsh
+curl -s https://raw.githubusercontent.com/DrgnFireYellow/zlugin/main/zlugin.zsh > $HOME/.zlugin/zlugin.zsh
 echo "source $HOME/.zlugin/zlugin.zsh" >> $HOME/.zshrc
 echo "Installation complete!"
