5017df452bbfe2ebe43e5368e870bfc4670b7bf4
DrgnFireYellow <86269995+DrgnFireYellow@users.noreply.github.com>
2022-09-15 17:25:40 +0000

    Update installer

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-echo "Press enter to install zlugin"
-read start
+echo "Installing zlugin"
 mkdir $HOME/.zlugin $>> $HOME/zlugin.log
 curl https://raw.githubusercontent.com/DrgnFireYellow/zlugin/main/zlugin.zsh > $HOME/.zlugin/zlugin.zsh
 echo "source $HOME/.zlugin/zlugin.zsh" >> $HOME/.zshrc
