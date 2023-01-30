bfe6142ce9298b3c37b8b168be842277f32d2b3e
DrgnFireYellow <86269995+DrgnFireYellow@users.noreply.github.com>
2023-01-30 16:03:19 +0000

    document zlugin.zsh

--- zlugin.zsh
+++ zlugin.zsh
@@ -1,4 +1,6 @@
 function zlugin() {
+    # Command to download and/or enable a plugin
+    # Syntax: zlugin [string: github repository of a plugin]
     git clone "https://github.com/$1.git" $HOME/.zlugin/$1 &>> $HOME/zlugin.log
     source $HOME/.zlugin/$1/*.*zsh*
 }
