5a7f6fa8a42b4e201bab6a35c8695c6519a62f81
DrgnFireYellow <86269995+DrgnFireYellow@users.noreply.github.com>
2023-01-30 18:13:08 +0000

    Create an update script to update zlugin Fixes #6

--- zlugin.zsh
+++ zlugin.zsh
@@ -2,3 +2,9 @@ function zlugin() {
     git clone "https://github.com/$1.git" $HOME/.zlugin/$1 &>> $HOME/zlugin.log
     source $HOME/.zlugin/$1/*.*zsh*
 }
+
+function update_zlugin() {
+    echo "Updating zlugin"
+    curl -s https://raw.githubusercontent.com/DrgnFireYellow/zlugin/main/zlugin.zsh > $HOME/.zlugin/zlugin.zsh
+    echo "Update complete!"
+}
