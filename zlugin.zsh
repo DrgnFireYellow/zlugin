a833fa7458e035d1fe3feabad76db5e4e7fd6687
DrgnFireYellow <86269995+DrgnFireYellow@users.noreply.github.com>
2023-04-02 11:26:45 -0400

    Add zlugin_reload and update documentation

--- zlugin.zsh
+++ zlugin.zsh
@@ -1,11 +1,18 @@
-function zlugin() {
-    # Command to download and/or enable a plugin
-    # Syntax: zlugin [string: github repository of a plugin]
-    git clone "https://github.com/$1.git" $HOME/.zlugin/$1 &>> $HOME/zlugin.log
+function zlugin_reload() {
+    # Initializes all downloaded plugins
     source $HOME/.zlugin/$1/*.*zsh*
 }
 
+function zlugin() {
+    # Downloads a plugin and reloads
+    # Syntax: zlugin [string: github repository of a plugin]
+    git clone "https://github.com/$1.git" $HOME/.zlugin/$1 &>> $HOME/zlugin.log
+    zlugin_reload
+}
+
 function update_zlugin() {
+    # Updates to the latest version of zlugin
+    # NOTE: The update will not take effect until you restart your shell
     echo "Updating zlugin"
     curl -s https://raw.githubusercontent.com/DrgnFireYellow/zlugin/main/zlugin.zsh > $HOME/.zlugin/zlugin.zsh
     echo "Update complete!"
